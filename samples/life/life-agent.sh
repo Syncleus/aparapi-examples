2bcf1ac950d88b993d2c643236dba6f1acf3d7d1
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2016-10-17 13:24:15 -0400

    Moved life sample out to the functioning part of the project.

--- samples/life/life-agent.sh
+++
@@ -1,6 +0,0 @@
-java\
- -agentpath:../../com.syncleus.aparapi.jni/dist/libaparapi_x86_64.so\
- -Dcom.syncleus.aparapi.executionMode=$1\
- -Dcom.syncleus.aparapi.useAgent=true\
- -classpath ../../com.syncleus.aparapi/dist/aparapi.jar:life.jar\
- com.syncleus.aparapi.sample.life.Main
